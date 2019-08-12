f4fcc89a83fe2c405e45660496dee77476b7179f
José Santiago Molano Perdomo <santi-1524@hotmail.com>
2019-08-12 07:40:43 -0500

    se agrega un espacio.
    Signed-off-by: José Santiago Molano Perdomo <santi-1524@hotmail.com>

--- src/main/java/com/qvision/screenplay/exceptions/ActorCannotQueryDatabaseException.java
+++ src/main/java/com/qvision/screenplay/exceptions/ActorCannotQueryDatabaseException.java
@@ -2,6 +2,6 @@ package com.qvision.screenplay.exceptions;
 
 public class ActorCannotQueryDatabaseException extends RuntimeException {
     public ActorCannotQueryDatabaseException(String name) {
-        super("The actor" + name + " does not have the ability to query the database");
+        super("The actor " + name + " does not have the ability to query the database");
     }
 }
