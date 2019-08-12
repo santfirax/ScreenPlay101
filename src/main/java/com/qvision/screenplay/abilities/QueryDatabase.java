61a0d16463b10bfe7a6f321530adacf7d419f5e8
José Santiago Molano Perdomo <santi-1524@hotmail.com>
2019-08-12 07:01:03 -0500

    Se agrega factory method y constructor privado
    Signed-off-by: José Santiago Molano Perdomo <santi-1524@hotmail.com>

--- src/main/java/com/qvision/screenplay/abilities/QueryDatabase.java
+++ src/main/java/com/qvision/screenplay/abilities/QueryDatabase.java
@@ -3,4 +3,18 @@ package com.qvision.screenplay.abilities;
 import net.serenitybdd.screenplay.Ability;
 
 public class QueryDatabase implements Ability {
+    private final String urlDatabase;
+    private final String username;
+    private final String password;
+
+    private QueryDatabase(String urlDatabase, String username, String password) {
+        this.urlDatabase = urlDatabase;
+        this.username = username;
+        this.password = password;
+    }
+
+    public static QueryDatabase configDatabase(String urlDatabase, String username, String password) {
+        return new QueryDatabase(urlDatabase, username, password);
+    }
+
 }
