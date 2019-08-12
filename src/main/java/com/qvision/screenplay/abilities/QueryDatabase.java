013db1a68802b9326deaca0aa5dc6f2218b14647
José Santiago Molano Perdomo <santi-1524@hotmail.com>
2019-08-12 07:21:35 -0500

    Se añade como devolver una habilidad.
    Signed-off-by: José Santiago Molano Perdomo <santi-1524@hotmail.com>

--- src/main/java/com/qvision/screenplay/abilities/QueryDatabase.java
+++ src/main/java/com/qvision/screenplay/abilities/QueryDatabase.java
@@ -1,11 +1,14 @@
 package com.qvision.screenplay.abilities;
 
 import net.serenitybdd.screenplay.Ability;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.RefersToActor;
 
-public class QueryDatabase implements Ability {
+public class QueryDatabase implements Ability, RefersToActor {
     private final String urlDatabase;
     private final String username;
     private final String password;
+    private Actor actor;
 
     private QueryDatabase(String urlDatabase, String username, String password) {
         this.urlDatabase = urlDatabase;
@@ -17,4 +20,14 @@ public class QueryDatabase implements Ability {
         return new QueryDatabase(urlDatabase, username, password);
     }
 
+    public static QueryDatabase as(Actor actor) {
+        return actor.abilityTo(QueryDatabase.class).asActor(actor);
+
+    }
+
+    @Override
+    public <T extends Ability> T asActor(Actor actor) {
+        this.actor = actor;
+        return (T) this;
+    }
 }
